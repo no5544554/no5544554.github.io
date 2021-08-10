bfe4ba082d1f0b5fef1f0d38713ab55cb9f5453a
Paul Lapsley <lapsley.p@gmail.com>
2021-08-10 00:35:37 -0500

    Changes

--- script/automatic.js
+++ script/automatic.js
@@ -1,20 +1,16 @@
-
-
-
 window.addEventListener("load",
     function() {
         document.title = "DFT Software";
-        
+
         document.getElementById("header").innerHTML = `
             <h1>DFT Software</h1>
         `;
 
-
         document.getElementById("navbar").innerHTML = `
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="testlink.html">Test Link</a></li>
-            </ul>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="testlink.html">Test Link</a></li>
+        </ul>
         `;
     }
-);
+)
