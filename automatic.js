e1a64c3f06460418c0029bccc10fa3b8e77854b3
Paul Lapsley <lapsley.p@gmail.com>
2021-08-10 00:20:56 -0500

    JS test

---
+++ automatic.js
@@ -0,0 +1,20 @@
+
+
+
+window.addEventListener("load",
+    function() {
+        document.title = "DFT Software";
+        
+        document.getElementById("header").innerHTML = `
+            <h1>DFT Software</h1>
+        `;
+
+
+        document.getElementById("navbar").innerHTML = `
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="testlink.html">Test Link</a></li>
+            </ul>
+        `;
+    }
+);
