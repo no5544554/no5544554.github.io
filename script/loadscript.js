87695e30b0896ba53511e38240d0e9dd8e9b8659
Paul Lapsley <lapsley.p@gmail.com>
2021-08-10 12:28:39 -0500

    Fix js

--- script/loadscript.js
+++ script/loadscript.js
@@ -1,3 +1,24 @@
 document.getElementById("header").innerHTML = `
     <h1>DFT Software</h1>
 `;
+
+
+
+
+
+
+window.addEventListener("load",
+    function()
+    {
+        document.getElementById("header").innerHTML = `
+            <h1>DFT Software</h1>
+        `;
+
+        document.getElementById("navbar").innerHTML = `
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="testlink.html">Test</a></li>
+            </ul>
+        `;
+    }
+);
